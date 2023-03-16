e7d38cfcaf0d78b1d075f1cbc2bbfdce3f207654
Harrison Senesac <harrisonas18@gmail.com>
2023-03-16 13:03:55 -0400

    Created Project

---
+++ OculoTakeHome/OculoTakeHome/OculoTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OculoTakeHomeApp.swift
+//  OculoTakeHome
+//
+//  Created by Harrison Senesac on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct OculoTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
